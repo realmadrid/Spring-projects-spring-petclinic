9792ee548bbee9cc9938aa74abfc2e7e5c0168c3
Peter Schrammel <peter.schrammel@diffblue.com>
2020-03-06 11:14:16 +0000

    Tests for "Merge pull request #586"
    https://github.com/diffblue/cover/commit/9a228202f90bb672f704e2a16a7325755f0b3aa6

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerDiffblueTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerDiffblueTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.WebDataBinder;
 public class OwnerControllerDiffblueTest {
   @Autowired
   private OwnerController ownerController;
+  @MockBean
+  private OwnerRepository ownerRepository;
   @Autowired
   private MockMvc mockMvc;
   @Test
@@ -38,6 +41,19 @@ public class OwnerControllerDiffblueTest {
   public void initFindFormTest() throws Exception {
     // Arrange
     MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/owners/find");
+
+    // Act
+    ResultActions actualPerformResult = this.mockMvc.perform(requestBuilder);
+
+    // Assert
+    ResultActions resultActions = actualPerformResult.andExpect(MockMvcResultMatchers.status().isOk());
+    ResultActions resultActions1 = resultActions.andExpect(MockMvcResultMatchers.model().<Object>size(1));
+    resultActions1.andExpect(MockMvcResultMatchers.model().attributeExists("owner"));
+  }
+  @Test
+  public void initUpdateOwnerFormTest() throws Exception {
+    // Arrange
+    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/owners/{ownerId}/edit", 1);
 
     // Act
     ResultActions actualPerformResult = this.mockMvc.perform(requestBuilder);
@@ -74,6 +90,19 @@ public class OwnerControllerDiffblueTest {
     resultActions1.andExpect(MockMvcResultMatchers.model().attributeExists("owner"));
   }
   @Test
+  public void processUpdateOwnerFormTest() throws Exception {
+    // Arrange and Act
+    ResultActions actualPerformResult = this.mockMvc
+        .perform(MockMvcRequestBuilders.post("/owners/{ownerId}/edit", 1).param("address", "a value for address")
+            .param("city", "a value for city").param("telephone", "a value for telephone")
+            .param("firstName", "a value for firstName").param("lastName", "a value for lastName"));
+
+    // Assert
+    ResultActions resultActions = actualPerformResult.andExpect(MockMvcResultMatchers.status().isOk());
+    ResultActions resultActions1 = resultActions.andExpect(MockMvcResultMatchers.model().<Object>size(1));
+    resultActions1.andExpect(MockMvcResultMatchers.model().attributeExists("owner"));
+  }
+  @Test
   public void setAllowedFieldsTest() {
     // Arrange
     WebDataBinder webDataBinder = new WebDataBinder("!");
@@ -83,6 +112,19 @@ public class OwnerControllerDiffblueTest {
 
     // Assert
     assertEquals(1, webDataBinder.getDisallowedFields().length);
+  }
+  @Test
+  public void showOwnerTest() throws Exception {
+    // Arrange
+    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/owners/{ownerId}", 1);
+
+    // Act
+    ResultActions actualPerformResult = this.mockMvc.perform(requestBuilder);
+
+    // Assert
+    ResultActions resultActions = actualPerformResult.andExpect(MockMvcResultMatchers.status().isOk());
+    ResultActions resultActions1 = resultActions.andExpect(MockMvcResultMatchers.model().<Object>size(1));
+    resultActions1.andExpect(MockMvcResultMatchers.model().attributeExists("owner"));
   }
 }
 
